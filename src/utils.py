cb73cf55074514dc67f1fab21d290e6ecc298148
Tafique <tafiquehossain2003@gmail.com>
2023-09-18 23:59:55 +0530

    Data injection and Data Transformation

---
+++ src/utils.py
@@ -0,0 +1,15 @@
+import os,sys
+from src.exception import CustomException
+from src.logger import logging
+
+import pickle
+
+def save_obj(file_path,obj):
+
+    try:
+        os.makedirs(os.path.dirname(file_path),exist_ok=True)
+
+        with open(file_path,'wb') as f:
+            pickle.dump(obj,f)
+    except Exception as e:
+        raise CustomException(e,sys)
